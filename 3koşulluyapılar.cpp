f2046c9d5af50dcacf70041acd8b513383b5fb43
Ykedi <yozanakkeci@gmail.com>
2024-10-27 14:09:08 +0300

    Update 3koşulluyapılar.cpp

--- 3koşulluyapılar.cpp
+++ 3koşulluyapılar.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+  int sayi=99;
+  if(sayi=2)
+{
+  cout<<"sayi cevaba cok yakin";
+}
+    if else(sayi=3)
+{
+  cout<<"sayi dogru";
+}
+  else
+{
+  cout<<"sayi hatalı";
+}
+}
